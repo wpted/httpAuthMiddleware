73e5c0dd575afce247ada1edc406e0369654c189
wpted <silverhandsome@gmail.com>
2023-03-16 12:50:58 +0800

    Add TestBasicAuthentication

--- middlewares/BasicAuthentication/basic_test.go
+++ middlewares/BasicAuthentication/basic_test.go
@@ -1,9 +1,13 @@
 package basicAuth
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestNewBasicConfig(t *testing.T) {
-	t.Run("Error - empty fields", func(t *testing.T) {
+	t.Run("Error-empty fields", func(t *testing.T) {
 		testUsername, testPassword := "", ""
 		_, err := NewBasicConfig(testUsername, testPassword)
 		if err != ErrFieldEmpty {
@@ -30,5 +34,58 @@ func TestNewBasicConfig(t *testing.T) {
 }
 
 func TestBasicAuthentication(t *testing.T) {
+	// app basic config
+	bc, err := NewBasicConfig("test", "test")
+	if err != nil {
+		t.Fatalf("Error creating BasicConfig: %v", err)
+	}
 
+	// handler to be served after passing the middleware
+	nextHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}
+
+	var username = "test1"
+	var password = "test1"
+
+	t.Run("Error-Incorrect username or password", func(t *testing.T) {
+		// user request
+		req := httptest.NewRequest(http.MethodGet, "/testing", nil)
+		req.SetBasicAuth(username, password)
+
+		// Use the middleware
+		testHandler := bc.BasicAuthentication(nextHandler)
+
+		// response recorder
+		rr := httptest.NewRecorder()
+
+		testHandler.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusUnauthorized {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				rr.Code, http.StatusUnauthorized)
+		}
+	})
+
+	username = "test"
+	password = "test"
+
+	t.Run("Success-Correct username and password", func(t *testing.T) {
+		// user request
+		req := httptest.NewRequest(http.MethodGet, "/testing", nil)
+		req.SetBasicAuth(username, password)
+
+		// Use the middleware
+		testHandler := bc.BasicAuthentication(nextHandler)
+
+		// response recorder
+		rr := httptest.NewRecorder()
+
+		testHandler.ServeHTTP(rr, req)
+
+		if rr.Code != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v",
+				rr.Code, http.StatusOK)
+		}
+	})
 }
